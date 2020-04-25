b9391127b856f06a85fcbe52e4d9eef4ab2f8ff9
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-24 22:03:03 -0300

    Style dashboard's repository

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,42 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório..." />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/29265857?s=460&u=439da134e6b1a4766dbba464f048a21cf2036f3e&v=4"
+            alt="User"
+          />
+          <div>
+            <strong>react-native-animations</strong>
+            <p>A bunch of examples on how to create awesome animations</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/29265857?s=460&u=439da134e6b1a4766dbba464f048a21cf2036f3e&v=4"
+            alt="User"
+          />
+          <div>
+            <strong>react-native-animations</strong>
+            <p>A bunch of examples on how to create awesome animations</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/29265857?s=460&u=439da134e6b1a4766dbba464f048a21cf2036f3e&v=4"
+            alt="User"
+          />
+          <div>
+            <strong>react-native-animations</strong>
+            <p>A bunch of examples on how to create awesome animations</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };

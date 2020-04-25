9dde96057e273d88db87559387979eb7db808951
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-24 23:35:28 -0300

    Render the entered repository using in the dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
-import logo from '../../assets/logo.svg';
+import api from '../../services/api';
 
+import logo from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repo {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  async function handleAddRepo(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repo>(`repos/${newRepo}`);
+    const repo = response.data;
+
+    setRepos([...repos, repo]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório..." />
+      <Form onSubmit={handleAddRepo}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório..."
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/29265857?s=460&u=439da134e6b1a4766dbba464f048a21cf2036f3e&v=4"
-            alt="User"
-          />
-          <div>
-            <strong>react-native-animations</strong>
-            <p>A bunch of examples on how to create awesome animations</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/29265857?s=460&u=439da134e6b1a4766dbba464f048a21cf2036f3e&v=4"
-            alt="User"
-          />
-          <div>
-            <strong>react-native-animations</strong>
-            <p>A bunch of examples on how to create awesome animations</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/29265857?s=460&u=439da134e6b1a4766dbba464f048a21cf2036f3e&v=4"
-            alt="User"
-          />
-          <div>
-            <strong>react-native-animations</strong>
-            <p>A bunch of examples on how to create awesome animations</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repos.map((repo) => (
+          <a key={repo.full_name} href="#">
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );

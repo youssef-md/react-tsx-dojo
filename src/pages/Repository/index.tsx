c47dfe03fa94e50556355fbb16becf14da172411
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-26 00:32:10 -0300

    Load repo data and its issues to render

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 import { Header, RepoInfo, Issues } from './styles';
 import logo from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 interface RepositoryParams {
   repo: string;
 }
 
+interface Repo {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
+
+  const [repo, setRepo] = useState<Repo | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    api.get(`repos/${params.repo}`).then((res) => setRepo(res.data));
+    api.get(`repos/${params.repo}/issues`).then((res) => setIssues(res.data));
+  }, [params.repo]);
 
   return (
     <>
@@ -21,59 +52,48 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </Header>
-      <RepoInfo>
-        <header>
-          <img src="https://avatars1.githubusercontent.com/u/29265857?v=4" />
-          <div>
-            <strong>youssef-md/tindev</strong>
-            <p>Descrição do repositórioooo</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1800</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepoInfo>
+
+      {repo && (
+        <RepoInfo>
+          <header>
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repo.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repo.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repo.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepoInfo>
+      )}
 
       <Issues>
-        <Link to="#aasd">
-          <div>
-            <strong>repo.full_name</strong>
-            <p>repo.description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link to="#aasd">
-          <div>
-            <strong>repo.full_name</strong>
-            <p>repo.description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link to="#aasd">
-          <div>
-            <strong>repo.full_name</strong>
-            <p>repo.description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link to="#aasd">
-          <div>
-            <strong>repo.full_name</strong>
-            <p>repo.description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
+        {issues.map((issue) => (
+          <a
+            key={issue.id}
+            href={issue.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
